23142fbeb85890c3a63533ba93c24d5cb7ad4a22
Jake Mitchell <mitchellja97@gmail.com>
2018-11-29 22:56:40 -0500

    add createUser

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,21 @@ import * as admin from 'firebase-admin'
 
 admin.initializeApp()
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!")
+const db = admin.firestore()
+
+export const createUser = functions.auth.user().onCreate((user, context) => {
+  return db.collection('users').doc(user.uid).set({
+    'name': user.displayName,
+    'email': user.email,
+    'customer': [],
+    'manager': []
+  })
 })
+
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!")
+// })
+
+// export const provisionDevice = functions.https.onRequest((request, response) => {
+
+// })
